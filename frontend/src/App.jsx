355b69dd6522fc03162a8d40c1cb6583a2415b6c
techminds <aztechminds@gmail.com>
2025-02-11 12:01:12 +0100

    modified height of grid column for articles and weather

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,17 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
-
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-        <main className="flex-grow overflow-y-auto pb-20">
-      </main>
       
-      <Home />
+      {/* Main content should grow to fill space, without scrolling */}
+      <main className="flex-grow overflow-y-auto pb-4">
+        <Home />
+      </main>
 
-      <footer>
+      <footer className="mt-auto">
         <Footer />
       </footer>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,18 @@
 function Home() {
   return (
-    
-    <div className="min-h-screen flex flex-col mt-auto">
-      <h1 className="text-center font-bold text-4xl mb-6">
+    <div className="flex flex-col">
+      <h1 className="text-center font-bold text-4xl mt-6 mb-6">
         Welcome to AllerCheck
       </h1>
 
-      <div className="flex-1 grid grid-cols-3 gap-2 p-4">
+      <div className="flex-1 grid grid-cols-3 gap-2 p-4 min-h-96">
         <div className="bg-blue-100 p-6 text-xl font-bold col-span-2">
           Articles
         </div>
         <div className="bg-green-100 p-6 text-xl font-bold">Wetter</div>
       </div>
 
-      <div className="flex justify-center space-x-4 mb-6">
+      <div className="flex justify-center space-x-4 mt-6">
         <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition">
           Barcode Scanner
         </button>
