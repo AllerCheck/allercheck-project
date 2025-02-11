7c2948c39bf2caa601875a286156ae6304cafccf
techminds <aztechminds@gmail.com>
2025-02-11 20:31:37 +0100

    SCRUM-25 changed font-size to xl in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <header className="bg-gray-300 p-4 flex justify-between">
-      <h1 className="font-bold">AllerCheck</h1>
+      <h1 className="font-bold text-xl">AllerCheck</h1>
       <div>log in / user</div>
     </header>
   );
